f2d38c17040a5b5a8555b31d5c6f4b2f867f433e
fabyr <fabyr@proton.me>
2026-02-20 14:33:17 +0100

    Add .editorconfig

--- setup.bat
+++ setup.bat
@@ -1,8 +1,8 @@
 @echo off
 python --version && python -m venv env && CALL env\Scripts\activate && python -m pip install --upgrade pip && python -m pip install -r requirements.txt && (
-  echo DONE
+	echo DONE
 ) || (
-  echo FAILED
+	echo FAILED
 )
 echo Press ENTER to exit.
 pause >nul
